efae47ac9fb301e74f1bca3f33faf302e5244a60
Murilo Guerreiro <guerreiro_murilo@hotmail.com>
2019-12-15 13:47:25 -0600

    Creating Scripts

---
+++ Instalacao.bat
@@ -0,0 +1,10 @@
+python -m venv venv
+cd venv/Scripts
+activate
+cd ../..
+pip install Flask
+pip install flask-sqlalchemy
+pip install flask-migrate
+pip install flask-script
+pip install flask-wtf
+pip install flask-login
